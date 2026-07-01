12bb4758eef341ee0b1ac2a37033d0221c6e9d03
mrlearn <rizdevelop@gmail.com>
2026-07-01 12:51:55 +0300

    feat(web): added about section

---
+++ src/page/IndexPage/ui/About/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useLanguage } from "@/shared/context/LanguageContext";
+
+import "./style.scss";
+
+export default function About() {
+  const { t } = useLanguage();
+
+  return (
+    <section className="section about" id="about">
+      <div className="container">
+        <div className="about__inner">
+          <div className="about__meta">
+            <span className="about__tag">{t.about.tag}</span>
+          </div>
+
+          <div className="about__body">
+            <p className="about__lead">{t.about.lead}</p>
+            <p className="about__text">{t.about.text}</p>
+          </div>
+        </div>
+
+        <div className="about__divider" />
+      </div>
+    </section>
+  );
+}
